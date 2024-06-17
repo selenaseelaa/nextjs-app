363f246808f8d973a8da3368eb41058ec2ba69d9
unknown <martinacahya08@gmail.com>
2024-06-17 18:36:22 +0700

    change security groups

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "nextjs-app" {
   ami               = "ami-04b70fa74e45c3917"
   instance_type     = "t2.micro"
-  vpc_security_group_ids = ["vpc-0a89c09fe1a3fd0fe"]
+  vpc_security_group_ids = ["sg-01e75675a95dae3a9"]
   key_name          = "ec2-key"
 
 
