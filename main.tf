72d15e43461cd90eb1074d253c926bd13dca7b6d
unknown <martinacahya08@gmail.com>
2024-06-17 18:03:00 +0700

    change configuration in main.tf

--- main.tf
+++ main.tf
@@ -24,8 +24,9 @@ resource "aws_security_group" "nextjs-app" {
 resource "aws_instance" "nextjs-app" {
   ami               = "ami-04b70fa74e45c3917"
   instance_type     = "t2.micro"
-  vpc_security_group_ids = "vpc-0a89c09fe1a3fd0fe"
+  vpc_security_group_ids = ["vpc-0a89c09fe1a3fd0fe"]
   key_name          = "ec2-key"
+
 
   tags = {
     Name = "nextjs-app"
