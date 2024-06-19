74899350932b9a51efa061a23037dbb7cff2949b
unknown <martinacahya08@gmail.com>
2024-06-19 23:55:06 +0700

    bisa dong bang

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between items-center bg-red-600 px-8 py-3">
       <Link className="text-white font-bold" href={"/"}>
-        Integration PEESOK
+        Integration PEESOKK
       </Link>
       <Link href={"/addTopic"}>
       <button className="bg-white text-purple-600 font-bold py-2 px-4 rounded-md hover:bg-purple-100">
