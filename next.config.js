2f344f9e0a801136f642b355e86ff368815d6b2e
selenaseelaa <qudsiyah.zahrain@gmail.com>
2024-06-20 00:03:30 +0700

    add next.config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Add the following line to disable SWC
+  swcMinify: false,
   // You can add other Next.js configuration options here
 };
 
