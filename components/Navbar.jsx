6c214b24b3f322efd2835d80addc152338c6281c
unknown <martinacahya08@gmail.com>
2024-06-19 23:25:59 +0700

    bisa yok

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center bg-purple-600 px-8 py-3">
+    <nav className="flex justify-between items-center bg-red-600 px-8 py-3">
       <Link className="text-white font-bold" href={"/"}>
         Integration NANA
       </Link>
