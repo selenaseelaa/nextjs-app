c2f60718cdc6e09afaadd9d3e9bf1edfa7838191
selenaseelaa <qudsiyah.zahrain@gmail.com>
2024-06-26 11:48:39 +0700

    last commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <nav className="flex items-center px-10 py-3 h-[80px]">
       <img src="/logo.jpeg" alt="Logo" className="h-20 w-20" />
       <Link className="text-red font-bold" href={"/"}>  
-        Catatan Sesi Cuci Darah (Hemodialisis)
+        Catatan Sesi Cuci DARAH (Hemodialisis)
       </Link>
     </nav>
   );
--- models/topic.js
+++ models/topic.js
@@ -16,3 +16,4 @@ const topicSchema = new Schema(
 const Topic = mongoose.models.Topic || mongoose.model("Topic", topicSchema);
 
 export default Topic;
+
