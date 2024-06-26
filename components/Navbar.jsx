a9ae69dcd7ec1a306451c8d3d92c191ca5eac170
selenaseelaa <qudsiyah.zahrain@gmail.com>
2024-06-26 09:59:56 +0700

    text merah

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,8 +4,8 @@ export default function Navbar() {
   return (
     <nav className="flex items-center px-10 py-3 h-[80px]">
       <img src="/logo.jpeg" alt="Logo" className="h-20 w-20" />
-      <Link className="text-black font-bold" href={"/"}>  
-        Catatan Sesi Cuci Darah (Hemodialisiss)
+      <Link className="text-red font-bold" href={"/"}>  
+        Catatan Sesi Cuci Darah (Hemodialisis)
       </Link>
     </nav>
   );
