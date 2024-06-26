1b4d0b6a38343b5080094a41fc5a398f941c9813
unknown <martinacahya08@gmail.com>
2024-06-26 09:42:14 +0700

    ngetes aja

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 function Footer() {
   return (
     <footer className="flex justify-between items-center px-8 py-3">
-      <p className="text-gray">&copy; 2024 Catatan Sesi Cuci Darah (Hemodialisis). All rights reserved.</p>
+      <p className="text-gray">&copy; 2024 Catatan Sesi Cuci Darah (Hemodialisis). All rights reserved..</p>
     </footer>
   );
 }
